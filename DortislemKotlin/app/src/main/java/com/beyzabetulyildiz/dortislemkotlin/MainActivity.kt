0dc4babe0fab5443a07f36d969ae47509f7c07e1
Beyza Betül Yıldız <beyzabetuly@gmail.com>
2023-05-24 18:36:21 +0300

    Dort islem with kotlin
    An application that performs four operations based on two numbers entered from the user (with written Kotlin using Android Studio)

---
+++ DortislemKotlin/app/src/main/java/com/beyzabetulyildiz/dortislemkotlin/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.beyzabetulyildiz.dortislemkotlin
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    var sayi1 : Int? =null
+    var sayi2 : Int? =null
+    var sonuc : Int? =null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun topla (view: View) {
+
+
+        sayi1 = editText.text.toString().toIntOrNull()
+
+        sayi2 = editText2.text.toString().toIntOrNull()
+        if ( sayi1== null || sayi2== null) {
+            textView3.text= "Sayı girmeyi unutmayınız."
+
+        } else {   sonuc=sayi1!!+sayi2!!
+            textView3.text= "Sonuç:${sonuc}"
+
+        }
+    }
+
+    fun cikar (view: View) {
+
+         sayi1 = editText.text.toString().toIntOrNull()
+
+         sayi2 = editText2.text.toString().toIntOrNull()
+        if ( sayi1== null || sayi2== null) {
+            textView3.text= "Sayı girmeyi unutmayınız."
+
+        } else {   sonuc=sayi1!!-sayi2!!
+            textView3.text= "Sonuç:${sonuc}"
+
+        }
+    }
+
+    fun carp (view: View) {
+
+        sayi1 = editText.text.toString().toIntOrNull()
+
+         sayi2 = editText2.text.toString().toIntOrNull()
+        if ( sayi1== null || sayi2== null) {
+            textView3.text= "Sayı girmeyi unutmayınız."
+
+        } else {   sonuc=sayi1!!*sayi2!!
+            textView3.text= "Sonuç:${sonuc}"
+
+        }
+    }
+
+    fun bolme (view: View) {
+
+         sayi1 = editText.text.toString().toIntOrNull()
+
+        sayi2 = editText2.text.toString().toIntOrNull()
+        if ( sayi1== null || sayi2== null) {
+            textView3.text= "Sayı girmeyi unutmayınız."
+
+        } else { sonuc=sayi1!! / sayi2!!
+            textView3.text= "Sonuç:${sonuc}"
+
+        }
+    }
+}
+
+
